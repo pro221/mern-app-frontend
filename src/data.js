a17cc4e383ab09bb2d405cfdad17d1251c8d318b
probin bhowmick <119671621+pro221@users.noreply.github.com>
2023-05-20 17:18:20 +0530

    this is my frontend commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products:[
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand:' Nike',
+            rating: 4.8,
+            numReviews: 30,
+            description: 'good quality shirt'
+
+        },
+   
+        {
+            name: 'Adidas Fit shirt',
+            slug: 'Adiddas-fit-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: 20,
+            brand:' Adiddas',
+            rating: 4.6,
+            numReviews: 20,
+            description: 'high quality shirt'
+
+        },
+        {
+            name: 'Nike Slim pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 20,
+            countInStock: 15,
+            brand:' Nike',
+            rating: 4.8,
+            numReviews: 18,
+            description: 'good quality product'
+
+        },
+        {
+            name: 'Adidas Fit pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 70,
+            countInStock: 30,
+            brand:' Adiddas',
+            rating: 4.0,
+            numReviews: 80,
+            description: 'good quality product'
+
+        }
+    ]
+}
+export default data
